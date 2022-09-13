0efbae2d6c2fc4a9026f70d635bb1fd5da80174d
Cas Euser <99072283@mydavinci.nl>
2022-09-13 13:32:01 +0200

    module-2

---
+++ Module-2/feestlunch.py
@@ -0,0 +1,9 @@
+Croissant = 17.0
+prijs1 = 0.39 
+Stokbrood = 2.0
+prijs2 = 2.78 
+korting = 3.0
+kortingprijs = 0.50
+print(f'{Croissant * prijs1} euro')
+print(f'{Stokbrood * prijs2} euro')
+print(f'{Stokbrood * prijs2 + Croissant * prijs1 - korting * kortingprijs} euro')
