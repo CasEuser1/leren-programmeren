f019e8810df4e6865ce914bf22e5e0122444e29b
Cas Euser <99072283@mydavinci.nl>
2022-09-22 11:49:09 +0200

    Module-2

--- Module-2/smartphone.py
+++ Module-2/smartphone.py
@@ -1,19 +1,22 @@
-telefoon =int(input('Hoe duur is de iPhone 13?'))
+telefoon = int(input('Hoe duur is de iPhone 13?'))
 telefoon2 = int(input('Hoe duur is de Samnsung Galaxy S22?'))
+verschil = telefoon - telefoon2
 if telefoon > telefoon2:
     print(f'De iPhone is het duurst, de telefoon kost {telefoon}')
     print(f'De Samsung is het goedkoopst, de telefoon kost {telefoon2}')
+    print(f'Het verschil is {verschil}')
     print('Het advies is dus de Samsung. Deze is namelijk goedkoper dan de iPhone.')
-    if telefoon > 900:
-        print('De iPhone kost meer dan uw budget, ik raad aan de Samsung te kopen')
-elif telefoon2 > telefoon:
+if verschil < 50:
+    print('Het verschil is lager dan 50 euro, dus ik raad nogsteeds de iPhone aan.')
+
+verschil2 = telefoon2 - telefoon
+if telefoon2 > telefoon:
     print(f'De Samsung is het duurst,de telefoon kost {telefoon2}')
     print(f'De iPhone is het goedkoopst, de telefoon kost {telefoon}')
-    print('Het advies is dus de iPhone. Deze is namlijk goedkoper dan de Samsung.')
-    if telefoon2 > 900:
-        print('De Samsung kost meer dan uw budget,ik raad aan de iPhone te kopen ')
+    print(f'Het verschil is {verschil}')
+    print('Het advies is dus de iPhone. Deze is namelijk goedkoper dan da Samsung')
+    if verschil2 < 50:
+        print('Het verschil is lager dan 50 euro, dus ik raad nogsteeds de Samsung aan')
+
 elif telefoon == telefoon2:
     print('Ze kosten allebei evenveel, maar iPhone had uw voorkeur, dus ik raad de iPhone aan.')
-    if telefoon > 900 and telefoon2 > 900:
-        print('Beide telefoons zijn boven uw budget, kies een ander model of verhoog uw budget.')
-    
