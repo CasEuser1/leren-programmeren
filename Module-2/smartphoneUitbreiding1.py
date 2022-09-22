427ce7bc6f02c8dc4a9e899e60c968f9e67bb49b
Cas Euser <99072283@mydavinci.nl>
2022-09-22 10:19:07 +0200

    Module-2

--- Module-2/smartphoneUitbreiding1.py
+++ Module-2/smartphoneUitbreiding1.py
@@ -4,16 +4,19 @@ if telefoon > telefoon2:
     print(f'De iPhone is het duurst, de telefoon kost {telefoon}')
     print(f'De Samsung is het goedkoopst, de telefoon kost {telefoon2}')
     print('Het advies is dus de Samsung. Deze is namelijk goedkoper dan de iPhone.')
-    if telefoon > 900:
-        print('De iPhone kost meer dan uw budget, ik raad aan de Samsung te kopen')
+
 elif telefoon2 > telefoon:
     print(f'De Samsung is het duurst,de telefoon kost {telefoon2}')
     print(f'De iPhone is het goedkoopst, de telefoon kost {telefoon}')
     print('Het advies is dus de iPhone. Deze is namlijk goedkoper dan de Samsung.')
-    if telefoon2 > 900:
-        print('De Samsung kost meer dan uw budget,ik raad aan de iPhone te kopen ')
+
+if telefoon > 900:
+    print('Alleen de iPhone is te duur, kies de Samsung.')
+if telefoon2 > 900:
+    print('Alleen de Samsung is te duur, kies de iPhone.')
+
 elif telefoon == telefoon2:
     print('Ze kosten allebei evenveel, maar iPhone had uw voorkeur, dus ik raad de iPhone aan.')
-    if telefoon > 900 and telefoon2 > 900:
-        print('Beide telefoons zijn boven uw budget, kies een ander model of verhoog uw budget.')
+if telefoon > 900 and telefoon2 > 900:
+    print('Beide telefoons zijn boven uw budget, kies een ander model of verhoog uw budget.')
     
