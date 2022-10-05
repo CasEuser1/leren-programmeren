23bcf0a21db4bc306802791e0f375befd0279cf1
Cas Euser <99072283@mydavinci.nl>
2022-10-05 09:30:17 +0200

    Module-2

--- Module-2/PizzaCalculatorDeluxe.py
+++ Module-2/PizzaCalculatorDeluxe.py
@@ -4,15 +4,17 @@ try:
     PizzaSmall = int(input('Hoeveel Small pizzas van 25cm en 7$ wilt u?'))
 except ValueError:
     print('U heeft iets verkeerd ingetypt, gebruik alstublieft cijfers en geen letters.')
-    exit()
+    PizzaSmall = 0
 try:
     PizzaMedium = int(input('Hoeveel medium pizzas van 29 cm en 9$ wilt u?'))
 except ValueError:
     print('U heeft iets verkeerd ingetypt, gebruik alstublieft cijfers en geen letters.')
+    exit()
 try:
     PizzaLarge = int(input('Hoeveel large pizzas van 35 cm en 12$ wilt u?'))
 except ValueError:
     print('U heeft iets verkeerd ingetypt, gebruik alstublieft cijfers en geen letters.')
+    exit()
 
 SmallPrijs = 7
 MediumPrijs = 9
