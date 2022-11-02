04e112932a820fd033e14215fb6f2a99be49b5f7
Cas Euser <99072283@mydavinci.nl>
2022-11-02 13:40:35 +0100

    module-3

--- Module-2/gerard.py
+++ Module-2/gerard.py
@@ -1,6 +1,23 @@
+import random
 naam = input ('Wat is je naam')
-nummer = int(input('Hoevaak wil je dat je print je bent geweldig'))
+nummer = int(input('Hoeveel complimenten wil je?'))
 
 for x in range (nummer):
-    print(f'Je bent geweldig {naam}')
-    
+    hip = random.randint(1,4)
+    if hip == 1:
+        print(f'Je bent cool {naam}')
+        if hip == 1:
+            print(f'{2 or 3 or 4}')
+    elif hip == 2:
+        print(f'Je bent goofy ahh {naam}')
+        if hip == 2:
+            print(f'{1 or 3 or 4}')
+    elif hip == 3:
+        print(f'Je bent slim {naam}')
+        if hip == 3:
+            print(f'{1 or 2 or 4}')
+    elif hip == 4:
+        print(f'Je bent geweldig {naam}')
+        if hip == 4:
+            print(f'{1 or 2 or 3}')
+
---
+++ Module-3/robotarm-python-2021-main/robotarm5.py
@@ -0,0 +1,21 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 5')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.speed=2
+for x in range(1,8):
+    robotArm.moveRight()
+for x in range(7):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+    robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
