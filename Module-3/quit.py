4e266207d77c1df88c903e384e9c02c55144c658
Cas Euser <99072283@mydavinci.nl>
2022-11-04 09:29:01 +0100

    module-3

---
+++ Module-3/quit.py
@@ -0,0 +1,10 @@
+# stelt de vraag "?" aan de gebruiker
+# zolang er geen quit geantwoord word stel je de vraag opnieuw
+# print het aantal keer dat de vraag gesteld is
+aantal_keer = 0
+vraag = input("?")
+while vraag != "quit":
+    aantal_keer = aantal_keer + 1
+    vraag = input("?")
+
+print(f'{aantal_keer} pogingen.')
