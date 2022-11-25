35e570ee5b57c6b9a1688c4a78277b8c28b9873b
Cas Euser <99072283@mydavinci.nl>
2022-11-25 09:38:51 +0100

    Module-4

---
+++ Module-4/MnM.py
@@ -0,0 +1,12 @@
+from random import randint
+
+KleurMnM = ['bruin','oranje', 'blauw','groen']
+vraag = int(input('Hoeveel M&M wil je hebben?'))
+LegeList = []
+
+for x in range(vraag):
+    KleurVerschil = randint(0,3)
+    LegeList.append(KleurMnM)
+    RandomKLeur = KleurMnM[KleurVerschil]
+print(LegeList)
+
