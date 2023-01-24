0cabfd5dcf2e7da9b2410d156cd5f56c87c207d9
CasEuser1 <113170768+CasEuser1@users.noreply.github.com>
2023-01-24 14:43:57 +0100

    Module-5

---
+++ Module-5/NamenEnLeeftijd.py
@@ -0,0 +1,16 @@
+def naamleeftijd(x: list):
+    stop = True
+    while stop:
+        naam = input('Geef naam: ').capitalize()
+        leeftijd = input('Geef een leeftijd: ')
+        if naam == 'stop' or leeftijd == 'stop':
+            stop = False
+            return x
+        NamenEnLeeftijd.append({'naam': naam, 'leeftijd':leeftijd})
+
+NamenEnLeeftijd = []
+
+naamleeftijd(NamenEnLeeftijd)
+
+
+print(NamenEnLeeftijd)
---
+++ Module-5/NamenEnLeeftijdRework.py
@@ -0,0 +1,17 @@
+def naamleeftijd(x: list):
+    naam = input('Geef naam: ').capitalize()
+    leeftijd = input('Geef een leeftijd: ')
+    NamenEnLeeftijd.append({'naam': naam, 'leeftijd':leeftijd})
+    return x
+
+NamenEnLeeftijd = []
+stop = True
+while stop == True:
+    naamleeftijd(NamenEnLeeftijd)
+    stoppen = input('Toets enter om door te gaan of stop om te printen:')
+    if stoppen == 'stop':
+        stop = False      
+
+x = len(NamenEnLeeftijd)
+for aantallen in range(0, x):
+    print(f'{NamenEnLeeftijd[aantallen]["naam"]} is {NamenEnLeeftijd[aantallen]["leeftijd"]}')
