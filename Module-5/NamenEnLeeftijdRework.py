deb522b5ebcf2bcb43c075774953b8e4f4b35294
CasEuser1 <113170768+CasEuser1@users.noreply.github.com>
2023-01-27 09:47:23 +0100

    Module-5

--- Module-5/NamenEnLeeftijdRework.py
+++ Module-5/NamenEnLeeftijdRework.py
@@ -8,10 +8,10 @@ NamenEnLeeftijd = []
 stop = True
 while stop == True:
     naamleeftijd(NamenEnLeeftijd)
-    stoppen = input('Toets enter om door te gaan of stop om te printen:')
+    stoppen = input('Toets enter om door te gaan of stop om te printen:').lower
     if stoppen == 'stop':
         stop = False      
 
 x = len(NamenEnLeeftijd)
-for aantallen in range(0, x):
+for aantallen in range(0,):
     print(f'{NamenEnLeeftijd[aantallen]["naam"]} is {NamenEnLeeftijd[aantallen]["leeftijd"]}')
